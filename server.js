afd1631d36f8f756e702b7b65035658a61ca8687
Steve <shalomlandsman@gmail.com>
2018-10-11 03:54:54 -0400

    add nodemon, graphql running;

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var {
+  buildSchema
+} = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -11,12 +13,16 @@ var schema = buildSchema(`
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-      return 'Hello world!';
-    },
-  };
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response);
-  });
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
